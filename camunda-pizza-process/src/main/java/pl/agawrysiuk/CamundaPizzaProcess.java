f1e1886b630809d3b23c8bfa2ae5d3cd3afa3ddd
dlrzz <arkad.gawry@gmail.com>
2020-03-23 19:45:03 +0100

    Added Camunda Main class

---
+++ camunda-pizza-process/src/main/java/pl/agawrysiuk/CamundaPizzaProcess.java
@@ -0,0 +1,11 @@
+package pl.agawrysiuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaPizzaProcess {
+    public static void main(String... args) {
+        SpringApplication.run(CamundaPizzaProcess.class, args);
+    }
+}
