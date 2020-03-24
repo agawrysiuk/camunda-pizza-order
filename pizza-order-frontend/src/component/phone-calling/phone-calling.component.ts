d5f6524ab6f3cb45979328b3f565ac7b969e3daa
dlrzz <arkad.gawry@gmail.com>
2020-03-24 19:54:23 +0100

    Added phone-calling component

---
+++ pizza-order-frontend/src/component/phone-calling/phone-calling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-calling',
+  templateUrl: './phone-calling.component.html',
+  styleUrls: ['./phone-calling.component.css']
+})
+export class PhoneCallingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
