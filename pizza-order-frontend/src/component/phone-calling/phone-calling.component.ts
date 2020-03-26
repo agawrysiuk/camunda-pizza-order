e317483e5ef20e5711fa075cd157a9ee5127c168
dlrzz <arkad.gawry@gmail.com>
2020-03-26 22:29:32 +0100

    Change calling transition to calling animation

--- pizza-order-frontend/src/component/phone-calling/phone-calling.component.ts
+++ pizza-order-frontend/src/component/phone-calling/phone-calling.component.ts
@@ -7,12 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhoneCallingComponent implements OnInit {
 
+  private clicked: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  moveForward() {
-
+  moveForward(callingCircle: HTMLDivElement, callingImage: HTMLImageElement, callingText: HTMLDivElement) {
+    if(!this.clicked) {
+      callingCircle.className += ' callingCircleAfterClick';
+      callingImage.className += ' callingImageAfterClick';
+      callingText.className += ' callingTextAfterClick';
+      this.clicked = true;
+    }
   }
 }
