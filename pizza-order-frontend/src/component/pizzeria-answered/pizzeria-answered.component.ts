0989d3028a2dba1c16db969197b6ca4661c0c63b
dlrzz <arkad.gawry@gmail.com>
2020-03-28 14:05:36 +0100

    Added a response in pizzeria-answered.component

--- pizza-order-frontend/src/component/pizzeria-answered/pizzeria-answered.component.ts
+++ pizza-order-frontend/src/component/pizzeria-answered/pizzeria-answered.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -8,13 +8,20 @@ import {Router} from '@angular/router';
 })
 export class PizzeriaAnsweredComponent implements OnInit {
 
-  public answersMap: { [ index: number ]: string} = {
-    1 : 'Hello, I\'d like to order some pizza.',
-    2 : 'Oh, sorry, wrong number!',
-    3 : '... (uncomfortable silence)'
+  public answersMap: { [index: number]: string } = {
+    1: 'Hello, I\'d like to order some pizza.',
+    2: 'Oh, sorry, wrong number!',
+    3: '... (uncomfortable silence)'
   };
 
-  constructor(private router: Router) { }
+  public reactionMap: { [index: number]: string } = {
+    1: 'Superb choice! Please look at the various selection that will appear on your screen in a second.',
+    2: 'No problem. Have a nice day!',
+    3: 'Omg, I\'m calling the police!'
+  };
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
@@ -22,11 +29,18 @@ export class PizzeriaAnsweredComponent implements OnInit {
   chooseAction(key: string) {
     for (let mapKey in this.answersMap) {
       if (mapKey !== key) {
-        document.getElementById(mapKey).className = 'tile-hidden';
+        document.getElementById(mapKey).className = 'display-none';
       } else {
         document.getElementById(mapKey).className = 'conversation-tile';
         document.getElementById(mapKey + '-paragraph').className = 'your-answer';
       }
     }
+    this.createResponse(key);
+  }
+
+  private createResponse(key: string) {
+    document.getElementById('reaction-img').className = 'tile-hidden tile-img-start-animation';
+    document.getElementById('reaction-text').innerText = this.reactionMap[key];
+    document.getElementById('reaction-text').className = 'tile-hidden tile-text-start-animation';
   }
 }
