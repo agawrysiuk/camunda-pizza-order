e1e2bd63489b44399b6ef09eceabefd57699ce28
dlrzz <arkad.gawry@gmail.com>
2020-03-28 13:45:44 +0100

    Added answer animation

--- pizza-order-frontend/src/component/pizzeria-answered/pizzeria-answered.component.ts
+++ pizza-order-frontend/src/component/pizzeria-answered/pizzeria-answered.component.ts
@@ -8,9 +8,25 @@ import {Router} from '@angular/router';
 })
 export class PizzeriaAnsweredComponent implements OnInit {
 
+  public answersMap: { [ index: number ]: string} = {
+    1 : 'Hello, I\'d like to order some pizza.',
+    2 : 'Oh, sorry, wrong number!',
+    3 : '... (uncomfortable silence)'
+  };
+
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  chooseAction(key: string) {
+    for (let mapKey in this.answersMap) {
+      if (mapKey !== key) {
+        document.getElementById(mapKey).className = 'tile-hidden';
+      } else {
+        document.getElementById(mapKey).className = 'conversation-tile';
+        document.getElementById(mapKey + '-paragraph').className = 'your-answer';
+      }
+    }
+  }
 }
