daa7ffe29d54d39359c8d5422ded40a18a871084
dlrzz <arkad.gawry@gmail.com>
2020-04-01 20:48:16 +0200

    Added habarta's TypeScript Generator for simplicity

---
+++ pizza-order-frontend/src/model/dto.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+// Generated using typescript-generator version 2.21.588 on 2020-04-01 20:45:46.
+
+export interface DeliveryAddress {
+    name: string;
+    phone: string;
+    street: string;
+    building: string;
+    suite: string;
+    city: string;
+    zipCode: string;
+}
+
+export interface Pizza {
+    name: string;
+    photo: string;
+    components: string;
+    price: string;
+}
+
+export interface CamundaVariables {
+    processId: string;
+}
