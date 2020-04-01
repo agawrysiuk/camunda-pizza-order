59d2132ce87bda23f2088daa47f95b2c1817ab55
dlrzz <arkad.gawry@gmail.com>
2020-04-01 20:29:01 +0200

    Added extra variables to the start of the process

--- camunda-pizza-process/src/main/java/pl/agawrysiuk/camunda/dto/CamundaVariables.java
+++ camunda-pizza-process/src/main/java/pl/agawrysiuk/camunda/dto/CamundaVariables.java
@@ -1,10 +1,14 @@
 package pl.agawrysiuk.camunda.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class CamundaVariables {
 
     private String processId;
--- camunda-pizza-process/src/main/java/pl/agawrysiuk/process/ProcessManager.java
+++ camunda-pizza-process/src/main/java/pl/agawrysiuk/process/ProcessManager.java
@@ -2,8 +2,11 @@ package pl.agawrysiuk.process;
 
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.springframework.stereotype.Component;
+import pl.agawrysiuk.camunda.dto.CamundaVariables;
+import pl.agawrysiuk.utils.ConvertCamundaVariables;
+
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -11,8 +14,10 @@ public class ProcessManager {
 
     private final RuntimeService runtimeService;
 
-    public void startProcess(String processName) {
-//        runtimeService.startProcessInstanceById(processName);
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processName);
+    public CamundaVariables startProcess(String processName) {
+        CamundaVariables variables = new CamundaVariables();
+        Map<String, Object> variablesMap = ConvertCamundaVariables.convertToMap(variables);
+        runtimeService.startProcessInstanceByKey(processName, variablesMap);
+        return variables;
     }
 }
