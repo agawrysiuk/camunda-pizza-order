c1d17429135f7fb11b90e85554ab675af90bc7e8
dlrzz <arkad.gawry@gmail.com>
2020-04-04 14:05:15 +0200

    Added change step listener to notify frontend about changing steps in camunda

--- camunda-pizza-process/src/main/java/pl/agawrysiuk/delegates/DelegateConfiguration.java
+++ camunda-pizza-process/src/main/java/pl/agawrysiuk/delegates/DelegateConfiguration.java
@@ -2,12 +2,18 @@ package pl.agawrysiuk.delegates;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.agawrysiuk.delegates.servicetasks.ChangeStepListener;
 import pl.agawrysiuk.delegates.servicetasks.CheckDataDelegate;
 import pl.agawrysiuk.delegates.servicetasks.CreateVariablesDelegate;
 import pl.agawrysiuk.delegates.servicetasks.PhoneAnswerDelegate;
 
 @Configuration
 public class DelegateConfiguration {
+
+    @Bean
+    public ChangeStepListener changeStepListener() {
+        return new ChangeStepListener();
+    }
 
     @Bean
     public PhoneAnswerDelegate phoneAnswerDelegate() {
---
+++ camunda-pizza-process/src/main/java/pl/agawrysiuk/delegates/servicetasks/ChangeStepListener.java
@@ -0,0 +1,11 @@
+package pl.agawrysiuk.delegates.servicetasks;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+
+public class ChangeStepListener implements ExecutionListener {
+    @Override
+    public void notify(DelegateExecution execution) throws Exception {
+
+    }
+}
