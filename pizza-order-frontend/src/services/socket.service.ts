15fa792b2d53f65d135e994f3568d64af5e20081
dlrzz <arkad.gawry@gmail.com>
2020-04-04 13:52:09 +0200

    Implemented subscription to the websocket

--- pizza-order-frontend/src/services/socket.service.ts
+++ pizza-order-frontend/src/services/socket.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
-import {Stomp} from '@stomp/stompjs';
+import {Frame, Stomp} from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import {Message} from "@stomp/stompjs/esm5/i-message";
+import {CompatClient} from "@stomp/stompjs/esm5/compatibility/compat-client";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
 
-  private client;
+  private client: CompatClient;
   private readonly serverUrl: string;
 
   constructor() {
@@ -17,6 +19,12 @@ export class SocketService {
   public initializeWebSocketConnection(processId: string) {
     const webSocket = new SockJS(this.serverUrl);
     this.client = Stomp.over(webSocket);
-    this.client.connect();
+    this.client.connect({}, this.onConnect);
+  }
+
+  private onConnect = (frame?: Frame) => {
+    this.client.subscribe('/next', (message: Message) => {
+      console.log('Got a message: ' + message);
+    });
   }
 }
