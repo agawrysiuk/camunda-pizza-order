71a5a41d7ee324021b35708aa01263b06fd0736e
dlrzz <arkad.gawry@gmail.com>
2020-04-05 10:13:27 +0200

    Fixed method in phone-calling.component

--- pizza-order-frontend/src/components/phone-calling/phone-calling.component.ts
+++ pizza-order-frontend/src/components/phone-calling/phone-calling.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -15,13 +15,13 @@ export class PhoneCallingComponent implements OnInit {
   ngOnInit() {
   }
 
-  moveForward(callingCircle: HTMLDivElement, callingImage: HTMLImageElement, callingText: HTMLDivElement, callingLine: HTMLHRElement, callingTab: HTMLDivElement) {
+  moveForward() {
     if (!this.clicked) {
-      callingCircle.className = 'calling-circle callingCircleAfterClick';
-      callingImage.className = 'calling-image callingImageAfterClick';
-      callingText.className = 'calling-text callingTextAfterClick';
-      callingLine.className = 'calling-line calling-end-delayed-animation';
-      callingTab.className = 'calling-tab calling-tab-end-animation';
+      document.getElementById('callingCircle').className = 'calling-circle callingCircleAfterClick';
+      document.getElementById('callingImage').className = 'calling-image callingImageAfterClick';
+      document.getElementById('callingText').className = 'calling-text callingTextAfterClick';
+      document.getElementById('callingLine').className = 'calling-line calling-end-delayed-animation';
+      document.getElementById('callingTab').className = 'calling-tab calling-tab-end-animation';
       this.clicked = true;
       setTimeout(() => {
           this.router.navigate(['pizzeria-answered']);
