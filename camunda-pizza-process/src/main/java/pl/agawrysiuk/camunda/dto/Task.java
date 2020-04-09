15c12272d0744d6a3eb8c85e672311e26c3a9ca4
dlrzz <arkad.gawry@gmail.com>
2020-04-09 20:40:10 +0200

    Added a Task class

---
+++ camunda-pizza-process/src/main/java/pl/agawrysiuk/camunda/dto/Task.java
@@ -0,0 +1,32 @@
+package pl.agawrysiuk.camunda.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+    private String id;
+    private String name;
+    private String assignee;
+    private String created;
+    private String due;
+    private String followUp;
+    private String delegationState;
+    private String description;
+    private String executionId;
+    private String owner;
+    private String parentTaskId;
+    private Long priority;
+    private String processDefinitionId;
+    private String processInstanceId;
+    private String caseDefinitionId;
+    private String caseInstanceId;
+    private String caseExecutionId;
+    private String taskDefinitionKey;
+    private Boolean suspended;
+    private String formKey;
+    private String tenantId;
+}
